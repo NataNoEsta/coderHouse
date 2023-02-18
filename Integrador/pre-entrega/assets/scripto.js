ea02415b7b14fd61f539485db0e3c31a1f246d22
nataberger <natasha.berger@innew.la>
2023-02-18 10:55:00 -0300

    Correcciones

--- Integrador/pre-entrega/assets/scripto.js
+++ Integrador/pre-entrega/assets/scripto.js
@@ -1,7 +1,7 @@
 // menu de cafe
 const cafecitos = [
     { name: "Espresso", precio: 250 },
-    { name: "Espreso Doble", precio: 350 },
+    { name: "Espresso Doble", precio: 350 },
     { name: "Machiatto", precio: 350 },
     { name: "Capuccino", precio: 400 },
     { name: "Cafe Latte", precio: 470},
@@ -18,29 +18,38 @@ function verMenu() {
 }
 function verPedido() {
     const ordenPedido = pedido.map(cafe => cafe.name + " - $" + cafe.precio + "\n");
-    alert("Su pedido:\n"+ ordenPedido.join(''));
-    console.log("Su pedido:\n" + ordenPedido.join(''));
+    alert("Detalle del pedido:\n"+ ordenPedido.join(''));
+    console.log("Detalle del pedido:\n" + ordenPedido.join(''));
 };
+function sumarOrden(){
+    let acc = 0;
+    const precios = pedido.map(el => acc += el.precio)
+    for (let i in precios) {
+        acc += precios[i];
+    }
+    return acc;
+}
 var elegir = prompt("Escriba su orden o [0] para salir.")
 
-while (elegir != "0"){
+while (elegir !== "0"){
     switch (elegir){
         case "1":
             verMenu();
             break;
         case "2":
-            var orden = prompt("Qué variedad de café desea? ")
+            var orden = prompt("Qué variedad de café desea? ");
             var resultado = cafecitos.find((el) => (el.name).toLowerCase() == orden);
             resultado ? pedido.push(resultado) : alert("No tenemos eso");
+            break
         case "0":
-            console.log("Gracias, vuelva pronto!");
+            alert("Gracias, Vuelva pronto")
             break
         default:
-            verPedido();
-            alert()
+            console.log("Gracias, vuelva pronto!");
+            alert("Gracias, Vuelva pronto")
             break;
     }
     elegir = prompt("Escriba su orden o [0] para salir.");  
 } 
-
+alert("La cuenta es: $"+sumarOrden());
 verPedido();
