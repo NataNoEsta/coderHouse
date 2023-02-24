b517aa06b2b0c35e2604d1fcfaf0c5b9a8ee5966
nataberger <natasha.berger@innew.la>
2023-02-24 07:11:39 -0300

    .

--- Integrador/pre-entrega-3/assets/script.js
+++ Integrador/pre-entrega-3/assets/script.js
@@ -13,13 +13,14 @@ const cafecitos = [
 localStorage.setItem("cafecitos", JSON.stringify(cafecitos));
 
 let carrito = [];
-const carritoBtn = document.getElementById("ver-carrito");
-carritoBtn.addEventListener('click', verCarrito);
+
+// const carritoBtn = document.getElementById("ver-carrito");
+// carritoBtn.addEventListener('click', verCarrito);
 
 const menuBtn = document.getElementById("ver-menu");
 menuBtn.addEventListener('click', verMenu);
 
-const pedidoBtn = document.getElementById("enviar");
+// const pedidoBtn = document.getElementById("enviar");
 
 function verMenu() {
     const menu = document.getElementById("menu")
@@ -36,25 +37,31 @@ function verMenu() {
 });
 }
 
+const btnAdd = document.querySelectorAll(".btn-add")
+for(let i = 0; i< btnAdd.length ; i++){
+    btnAdd.addEventListener('click', (ev) => {
 
-const formularioOrden = document.getElementById("formulario-de-orden");
-formularioOrden.addEventListener('submit', (ee) => {
-    console.log(ee.target[0].value)
-    let pedido = ee.target[0].value;
-        const buscado = cafecitos.find((item) => item.name.toLowerCase() == pedido.toLowerCase());
-        carrito.push(buscado);
+    })
 
-})
-
-function verCarrito() {
-    const supedido = document.getElementById("supedido")
-    const carritoOrden = carrito.map((carr) => {
-        supedido.innerHTML = `<article class="item-menu" id="item-menu">
-                            <h3 class="caf-title">${carr.name}</h3>
-                            <figure><img src=${carr.img}></figure>
-                            <p class="desc">${carr.descripcion}</p>
-                            <p class="precio">Precio: $${carr.precio}</p>  
-                            </article>`
-    });
-    
 }
+
+// const formularioOrden = document.getElementById("formulario-de-orden");
+// formularioOrden.addEventListener('submit', (ee) => {
+//     console.log(ee.target[0].value)
+//     let pedido = ee.target[0].value;
+//         const buscado = cafecitos.find((item) => item.name.toLowerCase() == pedido.toLowerCase());
+//         carrito.push(buscado);
+
+// })
+
+// function verCarrito() {
+//     const supedido = document.getElementById("supedido")
+//     const carritoOrden = carrito.map((carr) => {
+//         supedido.innerHTML = `<article class="item-menu" id="item-menu">
+//                             <h3 class="caf-title">${carr.name}</h3>
+//                             <figure><img src=${carr.img}></figure>
+//                             <p class="desc">${carr.descripcion}</p>
+//                             <p class="precio">Precio: $${carr.precio}</p>  
+//                             </article>`
+//     });   
+//}
