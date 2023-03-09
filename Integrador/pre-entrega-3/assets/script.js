4a177119f56d9ccdb4332242399580e721c75cd5
nataberger <natasha.berger@innew.la>
2023-03-09 00:11:51 -0300

    progreso en carritoy

--- Integrador/pre-entrega-3/assets/script.js
+++ Integrador/pre-entrega-3/assets/script.js
@@ -1,6 +1,4 @@
 // menu de cafe
-
-
 const cafecitos = [
     { name: "Espresso", precio: 250, descripcion: "Café negro", img: "/assets/images/espresso.png" },
     { name: "Espresso Doble", precio: 285, descripcion: "Café negro, doble", img: "/assets/images/espresso.png" },
@@ -12,53 +10,57 @@ const cafecitos = [
     { name: "Americano", precio: 270, descripcion: "Café mediano suave", img: "/assets/images/iced_coffee.png" },
     { name: "Mocachino", precio: 420, descripcion: "Capuchino con salsa de chocolate", img: "/assets/images/caffe_mochaccino.png" }
 ];
-localStorage.setItem("cafecitos", JSON.stringify(cafecitos));
-lista = JSON.parse(localStorage.getItem("cafecitos"));
+let carrito = []
+localStorage.setItem("carrito", JSON.stringify(carrito));
 
-let carrito = [];
+
+i=0;
+cafecitos.forEach((el) => el.id = i++)
+
+
 
 const carritoBtn = document.getElementById("ver-carrito");
-carritoBtn.addEventListener('click', verCarrito)
+carritoBtn.addEventListener('click', verCarrito(carrito))
 
 const menuBtn = document.getElementById("ver-menu");
-menuBtn.addEventListener('click', verMenu);
+menuBtn.addEventListener('click', itemMenu(cafecitos));
 
 function itemMenu(caf) {
-    return `<article class="item-menu">
-            <h3 class="caf-title">${caf.name}</h3>
-            <figure><img src=${caf.img} alt="ilust"></figure>
-            <p class="desc">${caf.descripcion}</p>
-            <p class="precio">Precio: $${caf.precio}</p>
-            <button class="btn-add" type="submit">Agregar</button>
-            </article>
-            `
-}
+    let app = document.getElementById('app');
+      for (let caf of cafecitos) {
+        let div = document.createElement('div');
+        div.innerHTML =`
+                    <article class="item-menu" id="">
+                    <h3 class="caf-title">${caf.name}</h3>
+                    <figure><img src=${caf.img} alt="ilust" id=""></figure>
+                    <p class="desc">${caf.descripcion}</p>
+                    <p class="precio">Precio: $${caf.precio}</p>
+                    <button class="btn-add" id="agregar${caf.name}${caf.id}" type="submit">Agregar</button>
+                    </article>`
+        
+        app.append(div);
+        let btnAdd = document.getElementById(`agregar${caf.name}${caf.id}`);
+        btnAdd.addEventListener('click', () => carrito.push(caf));  
+        console.log(carrito)
+    
+    }    
+};
 
-function verMenu() {
-    document.getElementById("app").innerHTML = `${lista.map(itemMenu).join('')}`
-}
-verMenu()
 
-const btnAdd = document.querySelectorAll('.btn-add');
-btnAdd.forEach((btn) => btn.addEventListener('click', agregarCarrito));
 
-function agregarCarrito(caf) {
-    let pedido = caf.target.parentElement;
-    console.log(pedido)
-    carrito.push({...lista[caf]});
-    console.log(carrito);
-    return carrito;
-}
+function verCarrito(carr) {  
+    let app = document.getElementById('pedido');
+    for(let carr of carrito){
+        let div = document.createElement('div');
+        div.innerHTML = ` <article class="item-menu" id="">
+                            <h3 class="caf-title">${carr.name}</h3>
+                            <figure><img src=${carr.img} alt="ilust" id=""></figure>
+                            <p class="desc">${carr.descripcion}</p>
+                            <p class="precio">Precio: $${carr.precio}</p>
+                            `
+        app.appendChild(div);
 
-function crearCarrito(carr) {
-    return `<article class="item-menu">
-            <h3 class="caf-title">${carr.name}</h3>
-            <figure><img src=${carr.img} alt="ilust"></figure>
-            <p class="desc">${carr.descripcion}</p>
-            <p class="precio">Precio: $${carr.precio}</p>
-            </article>
-            `
-}
-function verCarrito() {
-    document.getElementById("app").innerHTML = `${carrito.map(crearCarrito).join('')}`
-}
+    }
+    console.log(carrito)
+};
+carrito = JSON.parse(localStorage.getItem("carrito")) || [];
