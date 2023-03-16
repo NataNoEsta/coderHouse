989999b89ed6364a08ed3a3ca4e522b2f903962f
nataberger <natasha.berger@innew.la>
2023-03-16 11:41:58 -0300

    cambio estilos

--- Integrador/pre-entrega-3/assets/script.js
+++ Integrador/pre-entrega-3/assets/script.js
@@ -1,65 +1,62 @@
-// menu de cafe
-const cafecitos = [
-    { name: "Espresso", precio: 250, descripcion: "Café negro", img: "assets/images/espresso.png" },
-    { name: "Espresso Doble", precio: 285, descripcion: "Café negro, doble", img: "assets/images/espresso.png" },
-    { name: "Mediano", precio: 290, descripcion: "Café negro, 120ml", img: "assets/images/mediano.png" },
-    { name: "Machiatto", precio: 350, descripcion: "Café espresso con leche", img: "assets/images/caffe_mochaccino.png" },
-    { name: "Capuchino", precio: 400, descripcion: "Café con leche espumosa y canela", img: "assets/images/cappuccino.png" },
-    { name: "Cafe Latte", precio: 470, descripcion: "Café mediano con leche", img: "assets/images/caffe_latte.png" },
-    { name: "Iced Latte", precio: 470, descripcion: "Café mediano con leche y hielo", img: "assets/images/caffe_latte.png" },
-    { name: "Americano", precio: 270, descripcion: "Café mediano suave", img: "assets/images/iced_coffee.png" },
-    { name: "Mocachino", precio: 420, descripcion: "Capuchino con salsa de chocolate", img: "assets/images/caffe_mochaccino.png" }
-];
-
-// let carrito = [];
 let carrito = JSON.parse(localStorage.getItem("carrito")) || []
-localStorage.setItem("cafecitos", JSON.stringify(cafecitos))
 // localStorage.setItem("carrito", JSON.stringify(carrito));
 
-// genera un id para cada objeto en el array
-i=0;
-cafecitos.forEach((el) => el.id = i++)
-
-
-const carritoBtn = document.getElementById("ver-carrito");
-carritoBtn.addEventListener('click', verCarrito(carrito))
-
-const menuBtn = document.getElementById("ver-menu");
-menuBtn.addEventListener("onload", itemMenu(cafecitos));
-
-
-function itemMenu(cafecitos) {
+async function fetchdata() {
+    let response = await fetch("./data/data.json")
+    let data = await response.json();
     let app = document.getElementById('app');
-      for (let caf of cafecitos) {
+    for (let caf of data) {
+        console.log(caf)
         let div = document.createElement('div');
-        div.innerHTML =`
-                    <article class="item-menu">
-                    <h3 class="caf-title">${caf.name}</h3>
-                    <figure><img src=${caf.img} alt="ilust"></figure>
-                    <p class="desc">${caf.descripcion}</p>
-                    <p class="precio">Precio: $${caf.precio},00</p>
-                    <button class="btn-add" id="${caf.id}" type="submit">Agregar</button>
-                    </article>`
-        
+        div.innerHTML = `
+                        <article class="item-menu">
+                        <h3 class="caf-title">${caf.name}</h3>
+                        <figure><img src=${caf.img} alt="ilust"></figure>
+                        <p class="desc">${caf.descripcion}</p>
+                        <p class="precio">Precio: $${caf.precio},00</p>
+                        <button class="btn-add" id="${caf.id}" type="submit">Agregar</button>
+                        </article>`
+
         app.appendChild(div);
         let btnAdd = document.getElementById(`${caf.id}`);
         btnAdd.addEventListener('click', () => {
             // let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-            carrito.push(caf)
-            
-           localStorage.setItem("carrito", JSON.stringify(carrito)); 
+            let match = carrito.some((el) => caf.id === el.id)
+            let count = 0;
+            carrito.count = count
+            if (!match) {
+                carrito.push(caf)
+                console.log(carrito)
+            } else {
+                count++
+                console.log()
+            }
+
+
+            localStorage.setItem("carrito", JSON.stringify(carrito));
         });
-       
+
     }
-    // return carrito;
 };
 
-function verCarrito(carrito) {  
+fetchdata()
+
+const carritoBtn = document.getElementById("ver-carrito");
+
+carritoBtn.addEventListener('click', () => {
+    document.getElementById("sidebar").style = "display:block"
+})
+// const menuBtn = document.getElementById("ver-menu");
+// menuBtn.addEventListener("onload", fetchdata());
+
+
+
+function verCarrito(carrito) {
     let orden = document.getElementById('carrito');
-    
-    for(let el of carrito){
+
+    for (let el of carrito) {
         let div = document.createElement('div');
-        div.innerHTML = `<article class="item-menu">
+        div.innerHTML = `<article class="item-orden">
                     <h3 class="caf-title">${el.name}</h3>
                     <figure><img src=${el.img} alt="ilust"></figure>
                     <p class="desc">${el.descripcion}</p>
@@ -70,8 +67,9 @@ function verCarrito(carrito) {
     orden.style = "display: block"
 };
 
-if (carrito.length > 0 ){
+
+if (carrito.length > 0) {
     verCarrito(carrito);
-}else{
+} else {
     console.log('no hay carrito')
 }
---
+++ Integrador/pre-entrega-3/data/object.js
@@ -0,0 +1,18 @@
+class Cafe {
+    constructor(id, name, descripcion, img, precio, qty){
+        this.id = id;
+        this.name = name;
+        this.descripcion = descripcion;
+        this.img = img
+        this.precio = precio
+        this.qty = 0
+    }
+    setQty() {
+        return this.qty += 1;
+    }
+    getData() {
+        return `${this.id, this.name, this.descripcion, this.img, this.precio, this.qty}`
+    }
+}
+
+export{ setQty, getData }
