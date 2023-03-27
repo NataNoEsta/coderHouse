f611176a34abbaa1a1759d1cde84a72b870b4b80
nataberger <natasha.berger@innew.la>
2023-03-27 15:46:43 -0300

    dfdf

--- Integrador/final/assets/js/script.js
+++ Integrador/final/assets/js/script.js
@@ -1,31 +1,23 @@
-import { Cafe } from "/data/cafedata.js";
-
-let fr = new Cafe();
-console.log(fr);
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 // localStorage.setItem("carrito", JSON.stringify(carrito));
 
 let cont = JSON.parse(localStorage.getItem("cont")) || 0;
 
 // hace visible el carrito
+const contador = document.getElementById("item-count");
+cont > 0 ? (contador.innerText = `${cont}`) : 0;
+
 const carritoBtn = document.getElementById("ver-carrito");
-carritoBtn.addEventListener("click", () => {
-  document.getElementById("sidebar").style = "display:block";
-});
 
-// oculta el carrito al hacer click en el background
-document.addEventListener("mouseup", function (e) {
-  let aside = document.getElementById("sidebar");
-  if (!aside.contains(e.target)) {
-    aside.style = "display: none";
+let aviso = document.getElementById("guia");
+let aside = document.getElementById("sidebar");
+
+function emptyCart() {
+  if (carrito.length === 0) {
+    orden.innerText = "El carrito está vacío"
   }
-});
+};
 
-// función asíncrona // obtiene los datos de un JSON local
-async function fetchdata() {
-  let response = await fetch("./data/data.json");
-  let data = await response.json();
-  let app = document.getElementById("app");
 
   for (let caf of data) {
     let div = document.createElement("div");
@@ -38,22 +30,32 @@ async function fetchdata() {
                         <button class="btn-add" id="${caf.id}" type="submit">Agregar</button>
                         </article>`;
 
-    app.appendChild(div);
+// oculta el carrito al hacer click en el background
+// document.addEventListener("mouseup", function () {
+//   if (!aside.contains(e.target)) {
+//     aside.style = "visibility: hiden";
+//   }
+// });
 
-    let btnAdd = document.getElementById(`${caf.id}`);
-    btnAdd.addEventListener("click", () => {
-      // asigna a 'match' si el item con el id existe en el carrito
-      // si 'f' lo agrega, si no aumenta la cantidad (qty)
-      let match = carrito.some((el) => caf.id === el.id);
-      if (!match) {
-        carrito.push(caf);
-        cont += caf.qty;
-      } else {
-        caf.qty += 1;
-        cont++;
-      }
+    let reloj = document.getElementById("reloj");
 
-      // alert de 'sweetAlert // se dispara cada ez que se agrega un item
+    function getTime() {
+      const dt = luxon.DateTime.now();
+      const dtplus = dt.plus({ minutes: 30 });
+      return dtplus.toLocaleString(luxon.DateTime.TIME_SIMPLE);
+    }
+
+    function menuTemplate(p) {
+      return `
+  <article class="item-menu">
+  <h3 class="caf-title">${p.name}</h3>
+  <figure><img src=${p.img} alt="ilust"></figure>
+  <p class="desc">${p.descripcion}</p>
+  <p class="precio">Precio: $${p.precio},00</p>
+  <button class="btn-add" id="${p.id}" type="submit">Agregar</button>
+  </article>`;
+    }
+    const toast = () => {
       Swal.fire({
         title: "Gracias",
         text: "Su pedido ha sido recibido",
@@ -81,6 +83,7 @@ function verCarrito(carrito) {
 
   for (let el of carrito) {
     let div = document.createElement("div");
+    div.className = "ordenes";
     div.innerHTML = `<article class="item-orden">
                     <h3 class="caf-title">${el.name}</h3>
                     <figure><img src=${el.img} alt="ilust"></figure>
@@ -90,25 +93,37 @@ function verCarrito(carrito) {
                     `;
     orden.appendChild(div);
   }
-  // vacía el localstorage
-  let vaciarCarrito = document.getElementById("vaciar-carrito");
-  vaciarCarrito.addEventListener("click", () => {
-    carrito.length > 0 && localStorage.clear();
-  });
 
-  let reloj = document.getElementById("reloj");
-  reloj.innerText = `Su pedido estará a las: ${getTime()}`;
 }
-if (carrito.length != 0) {
-  let aviso = document.getElementById("guia");
-  aviso.style = "display:block";
+// vacía el localstorage
+
+
+
+let vaciarCarrito = document.getElementById("vaciar-carrito");
+vaciarCarrito.addEventListener("click", resetCart);
+
+function resetCart() {
+  if (carrito.length > 0) {
+    localStorage.clear();
+    orden.innerHTML = "<p>Su carrito está vacío</p>";
+  }
+  cont = 0
+  contador.innerText = 0;
+  vaciarCarrito.disabled = true;
 }
+// carritoBtn.addEventListener('click', (e) => {
+//   aside.classList.toggle("show");
+// });
+
+function visualizar() {
+  verCarrito() && emptyCart();
+
+}
+visualizar();
+// } else {
+//   orden.textContent = "Su carrito está vacío";
+// }
 // obtiene la hora actual cuando se hace un pedido y calcula 30 min a partir de esa hora
-function getTime() {
-  const dt = luxon.DateTime.now();
-  const dtplus = dt.plus({ minutes: 30 });
-  return dtplus.toLocaleString(luxon.DateTime.TIME_SIMPLE);
-}
 
 // contenido de carrito vacío
 // function carritoVacio() {
@@ -116,4 +131,3 @@ function getTime() {
 // }
 
 // mostrar carrito / contenido de carrito vacío
-carrito.length > 0 ? verCarrito(carrito) : "Su carrito está vacío";
